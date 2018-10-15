01effc79606ec5f4cdcf0f30e8ab897acefc450d
vbrinnel <vsbrin@gmail.com>
2018-10-15 19:01:01 +0200

    New T3 dataclasses: GlobalInfo and JournalUpdate

---
+++ src/ampel/base/dataclass/JournalUpdate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : ampel/pipeline/t3/data/JournalUpdate.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 15.10.2018
+# Last Modified Date: 15.10.2018
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Any, Dict, List, Union
+from pydantic.dataclasses import dataclass
+
+@dataclass
+class JournalUpdate:
+	tranId: Union[int, List[int]]
+	content: Dict[str, Any]
+	ext: bool = False
