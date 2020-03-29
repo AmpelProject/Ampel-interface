485a847fc725617a1be99e371c36c151657d6f9f
vbrinnel <valery.brinnel@gmail.com>
2020-03-29 12:16:06 +0200

    New TypedDict: StockRecord

---
+++ ampel/content/StockRecord.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-interface/ampel/content/StockRecord.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 28.12.2019
+# Last Modified Date: 01.03.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Any, Sequence, Union, Optional, Dict, TypedDict
+from ampel.types import StockId, ChannelId
+
+
+class StockRecord(TypedDict):
+
+	_id: StockId
+	tag: Optional[Sequence[Union[int, str]]]
+	channel: Optional[Sequence[ChannelId]]
+	journal: Sequence[Dict[str, Any]]
+	name: Optional[Sequence[Union[int, str]]]
+	modified: Dict[str, Any]
+	created: Dict[str, Any]
