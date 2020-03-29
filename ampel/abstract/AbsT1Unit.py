5432bf976543b495e361ef531730488a316b9dfb
vbrinnel <valery.brinnel@gmail.com>
2020-03-29 12:26:30 +0200

    New abstract class: AbsT1Unit

---
+++ ampel/abstract/AbsT1Unit.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-interface/ampel/abstract/AbsT1Unit.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 11.10.2019
+# Last Modified Date: 18.03.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Sequence, Generic, TypeVar, ClassVar, Type, Iterable
+from ampel.abc import abstractmethod
+from ampel.types import StockId, ChannelId
+from ampel.content.DataPoint import DataPoint
+from ampel.abstract.AbsDataUnit import AbsDataUnit
+from ampel.ingest.CompoundBluePrint import CompoundBluePrint
+
+T = TypeVar("T", bound=CompoundBluePrint)
+
+class AbsT1Unit(Generic[T], AbsDataUnit, abstract=True):
+
+	BluePrintClass: ClassVar[Type] = CompoundBluePrint
+
+	@abstractmethod
+	def combine(self,
+		stock_id: StockId,
+		datapoints: Sequence[DataPoint],
+		channels: Iterable[ChannelId]
+	) -> T:
+		...
