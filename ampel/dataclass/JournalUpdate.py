e556c8b5ca5febfe5ea8cbea6d6ab0d47ecb3615
vbrinnel <valery.brinnel@gmail.com>
2020-03-29 12:27:56 +0200

    JournalUpdate: field update

--- ampel/dataclass/JournalUpdate.py
+++ ampel/dataclass/JournalUpdate.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/base/dataclass/JournalUpdate.py
+# File              : Ampel-interface/ampel/dataclass/JournalUpdate.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 15.10.2018
-# Last Modified Date: 15.10.2018
+# Last Modified Date: 14.02.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Any, Dict, List, Union
 from pydantic.dataclasses import dataclass
+from ampel.types import ChannelId
 
-@dataclass
+
+@dataclass(frozen=True)
 class JournalUpdate:
-	tran_id: Union[int, List[int]]
+	id: Union[ChannelId, List[ChannelId]]
 	content: Dict[str, Any]
 	ext: bool = False
