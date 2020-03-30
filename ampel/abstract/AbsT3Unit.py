89911cae5b10c2c87e7a5a26eee2cf23c46b21c7
vbrinnel <valery.brinnel@gmail.com>
2020-03-30 18:20:33 +0200

    AbsT3Unit: v0.7 update

--- ampel/abstract/AbsT3Unit.py
+++ ampel/abstract/AbsT3Unit.py
@@ -1,28 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/base/abstract/AbsT3Unit.py
+# File              : Ampel-interface/ampel/abstract/AbsT3Unit.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 23.02.2018
-# Last Modified Date: 15.10.2018
+# Last Modified Date: 17.02.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from ampel.base.abstract.AmpelABC import AmpelABC, abstractmethod
+from typing import Sequence, Tuple, Optional, Dict, Any
+from ampel.abc import abstractmethod, defaultmethod
+from ampel.abstract.AbsDataUnit import AbsDataUnit
+from ampel.view.SnapView import SnapView
+from ampel.dataclass.JournalUpdate import JournalUpdate
 
-class AbsT3Unit(metaclass=AmpelABC):
-	"""
-	"""
 
-	resources = tuple()
+class AbsT3Unit(AbsDataUnit, abstract=True):
 
-	@abstractmethod
-	def __init__(self, logger, base_config=None, run_config=None, global_info=None):
+	@defaultmethod
+	def post_init(self, context: Optional[Dict[str, Any]]) -> None:
+		"""
+		Method called after contructor call by parent processor class
+
+		:param context: a dictionary containing context information,
+		which can be requested in the process configuration.
+		Examples of context information are:
+		- Date and time the current process was last run
+		- Number of alerts processed since then
+
+		Note: custom context information can be added to this dict by
+		implementing a class inheriting AbsT3ContextAppender (in ampel.abstract)
+		and by requesting the custom unit in the process configuration
+		"""
 		pass
 
-	@abstractmethod
-	def add(self, transients):
-		pass
 
 	@abstractmethod
-	def done(self):
+	def add(self, views: Tuple[SnapView, ...]) -> Sequence[JournalUpdate]:
+		""" Implementing T3 units get SnapViews via this this method """
+
+
+	@defaultmethod
+	def done(self) -> None:
+		""" Method called after all data have been processed """
 		pass
