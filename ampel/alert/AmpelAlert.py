bbd950fd51cafdc09ba2aeca121eab0345ed4876
vbrinnel <valery.brinnel@gmail.com>
2020-05-29 17:30:35 +0200

    AmpelAlert: remove kw_mappings and dataclass

--- ampel/alert/AmpelAlert.py
+++ ampel/alert/AmpelAlert.py
@@ -8,9 +8,11 @@
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import operator
-from dataclasses import dataclass
-from typing import Dict, Tuple, List, Sequence, Optional, Any, ClassVar, Set, Callable, Union
-from ampel.types import StockId
+from typing import Dict, Tuple, List, Sequence, Optional, Any, Callable, Union
+from ampel.type import StockId
+from ampel.content.DataPoint import DataPoint
+
+osa = object.__setattr__
 
 # Do not enable customizations of operators by sub-classes for now
 ops: Dict[str, Callable[[str, Any], bool]] = {
@@ -24,109 +26,114 @@ ops: Dict[str, Callable[[str, Any], bool]] = {
 	'is not': operator.is_not
 }
 
-@dataclass(frozen=True)
+def __ro__(self, *args, **kwargs):
+	raise RuntimeError("Cannot modify AmpelAlert")
+
+
 class AmpelAlert:
-	""" """
 
-	_kw_mappings: ClassVar[Dict[str, str]]
-	# static set is populated by __init_subclass__
-	_kws_set: ClassVar[Set[str]]
+	__slots__ = 'id', 'stock_id', 'dps', 'new'
+	__setattr__ = __ro__
 
+	id: int
 	stock_id: StockId
-	id: Union[int, str]
+	dps: Sequence[Dict]
 
-	# pylint: disable=unused-argument
-	def __init_subclass__(cls, **kwargs):
-		super().__init_subclass__(**kwargs)
-		if hasattr(cls, "_kw_mappings") and cls._kw_mappings is not None:
-			# Build set to optimize perf
-			cls._kws_set = set(cls._kw_mappings.keys())
-		else:
-			cls._kws_set = set()
+
+	def __init__(self, id: Union[int, str], stock_id: StockId, dps: Sequence[Dict]) -> None:
+		osa(self, 'id', id)
+		osa(self, 'stock_id', stock_id)
+		osa(self, 'dps', dps)
 
 
 	def get_values(self,
-		param_name: str,
-		# datapoints: Sequence[Dict],
-		field: str,
-		filters: Optional[Sequence[Dict[str, Any]]] = None
+		key: str,
+		filters: Optional[Sequence[Dict[str, Any]]] = None,
+		data: Optional[Sequence[Dict]] = None
 	) -> List[Any]:
 		""" ex: get_values("mag") """
 
-		if param_name in self._kws_set:
-			param_name = self._kw_mappings[param_name]
+		if not data:
+			data = self.dps
 
-		dps = self.apply_filter(getattr(self, field), filters) if filters else getattr(self, field)
-		return [el[param_name] for el in dps if param_name in el]
+		if filters:
+			data = AmpelAlert.apply_filter(data, filters)
+
+		return [el[key] for el in data if key in el]
 
 
 	def get_tuples(self,
-		param1: str, param2: str, field: str,
-		filters: Optional[Sequence[Dict[str, Any]]] = None
+		key1: str, key2: str,
+		filters: Optional[Sequence[Dict[str, Any]]] = None,
+		data: Optional[Sequence[Dict]] = None
 	) -> List[Tuple[Any, Any]]:
 		""" ex: get_tuples("obs_date", "mag") """
 
-		if param1 in self._kws_set:
-			param1 = self._kw_mappings[param1]
+		if not data:
+			data = self.dps
 
-		if param2 in self._kws_set:
-			param2 = self._kw_mappings[param2]
-
-		dps = self.apply_filter(getattr(self, field), filters) if filters else getattr(self, field)
+		if filters:
+			data = AmpelAlert.apply_filter(data, filters)
 
 		return [
-			(el[param1], el[param2])
-			for el in dps if param1 in el and param2 in el
+			(el[key1], el[key2])
+			for el in data if key1 in el and key2 in el
 		]
 
 
 	def get_ntuples(self,
-		params: List[str], field: str,
-		filters: Optional[Sequence[Dict[str, Any]]] = None
+		params: List[str],
+		filters: Optional[Sequence[Dict[str, Any]]] = None,
+		data: Optional[Sequence[Dict]] = None
 	) -> List[Tuple]:
 		""" ex: get_ntuples(["fid", "obs_date", "mag"]) """
 
-		# If any of the provided parameter matches defined keyword mappings
-		if self._kws_set & set(params):
-			params = params.copy()
-			for i, param in enumerate(params):
-				if param in self._kws_set:
-					params[i] = self._kw_mappings[param]
+		if not data:
+			data = self.dps
 
-		dps = self.apply_filter(getattr(self, field), filters) if filters else getattr(self, field)
+		if filters:
+			data = AmpelAlert.apply_filter(data, filters)
 
 		return [
 			tuple(el[param] for param in params)
-			for el in dps if all(param in el for param in params)
+			for el in data if all(param in el for param in params)
 		]
 
 
-	@classmethod
-	def apply_filter(cls,
+	def is_new(self) -> bool:
+		return len(self.dps) == 1
+
+
+	def dict(self) -> Dict[str, Any]:
+		return {'id': self.id, 'stock_id': self.stock_id, 'dps': self.dps}
+
+
+	@staticmethod
+	def apply_filter(
 		dicts: Sequence[Dict],
 		filters: Sequence[Dict[str, Any]]
 	) -> Sequence[Dict]:
-		"""
-		"""
 
 		if isinstance(filters, dict):
 			filters = [filters]
 		else:
 			if filters is None or not isinstance(filters, (list, tuple)):
-				raise ValueError("Filters must be dicts or list/tuple of dict")
+				raise ValueError("Parameter 'filters' must be a dict or a sequence of dicts")
 
-		for filtre in filters:
-
-			op = ops[filtre['operator']]
-			filter_attr_name = filtre['attribute']
-			attr_name = (
-				filter_attr_name if filter_attr_name not in cls._kws_set
-				else cls._kw_mappings[filter_attr_name]
-			)
-
-			dicts = [
-				d for d in dicts
-				if attr_name in d and op(d[attr_name], filtre['value'])
-			]
+		for f in filters:
+			op = ops[f['operator']]
+			f_attr = f['attribute']
+			f_val = f['value']
+			dicts = [d for d in dicts if f_attr in d and op(d[f_attr], f_val)]
 
 		return dicts
+
+
+	@staticmethod
+	def _prev_det_seq(datapoints: Sequence[DataPoint]) -> Optional[Sequence[DataPoint]]:
+		""" ampel.content.DataPoint are 'ampelized' dicts,
+		i.e not the 'raw' datapoints associated with in self.datapoints
+		"""
+		if len(datapoints) > 1:
+			return datapoints[1:]
+		return None
