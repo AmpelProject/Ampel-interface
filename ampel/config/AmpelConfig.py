4637c985991c504e262e44e2036a4414c3a45836
vbrinnel <valery.brinnel@gmail.com>
2020-05-29 17:27:20 +0200

    AmpelConfig: new constructor class methods

--- ampel/config/AmpelConfig.py
+++ ampel/config/AmpelConfig.py
@@ -4,63 +4,143 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 22.10.2019
-# Last Modified Date: 03.02.2020
+# Last Modified Date: 18.04.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-import json
-from typing import Dict, List, Union, Optional, TypeVar, Type, Literal, Any, overload
-from ampel.utils.Freeze import Freeze
+import yaml, json
+from typing import Dict, List, Union, Optional, Type, Literal, Any, Iterable, overload, get_origin
+from ampel.type import JT, JSONTypes
+from ampel.util.Freeze import Freeze
 from ampel.view.ReadOnlyDict import ReadOnlyDict
-
-T = TypeVar('T')
 
 
 class AmpelConfig:
-	"""
-	Class holding the central ampel configuration
-	"""
+	""" Class holding the central ampel configuration """
+
+	_config: Dict
+	_check_types: int = 1
 
 
-	def __init__(self, config: Dict, freeze: bool = True) -> None:
+	@classmethod
+	def new(cls,
+		config: Dict[str, Any],
+		pwd_file_path: Optional[str] = None,
+		pwds: Optional[Iterable[str]] = None,
+		freeze: bool = True
+	) -> 'AmpelConfig':
+
+		if pwd_file_path or pwds:
+
+			try:
+				from ampel.util.crypto import aes_recursive_decrypt
+			except Exception:
+				print("ampel-core is required for this feature")
+				return None # type: ignore
+
+			if pwd_file_path:
+				with open(pwd_file_path, "r") as f:
+					pwds = [l.strip() for l in f.readlines()]
+
+			config['resource'] = aes_recursive_decrypt(
+				config['resource'], pwds # type: ignore[arg-type]
+			)
+
+		return cls(config, freeze)
+
+
+	@classmethod
+	def load(cls,
+		config_file_path: str,
+		pwd_file_path: Optional[str] = None,
+		pwds: Optional[Iterable[str]] = None,
+		freeze: bool = True
+	) -> 'AmpelConfig':
+
+		with open(config_file_path, "r") as f:
+			return cls.new(yaml.safe_load(f), pwd_file_path, pwds, freeze)
+
+
+	def __init__(self, config: Dict, freeze: bool = False) -> None:
+		"""
+		:raises: ValueError if provided config is None or empty
+		"""
+		if config is None or not config:
+			raise ValueError("Please provide a config")
+
 		self._config: Dict = Freeze.recursive_freeze(config) if freeze else config
 
+		if 'general' in config and 'check_types' in config['general']:
+			self._check_types = config['general']['check_types']
+
+		# try to automatically register aux units
+		try:
+			from ampel.core.UnitLoader import UnitLoader
+			UnitLoader.aux_defs = config['unit'].get('aux')
+		except Exception:
+			print("UnitLoader auxiliary units auto-registration failed")
+
+	# Overloads for method call without 'entry'
+
+	@overload
+	def get(self) -> Dict[str, Any]:
+		""" config.get() """
 
 	@overload
 	def get(self, entry: None) -> Dict[str, Any]:
-		...
+		""" config.get(None) """
 
 	@overload
-	def get(self, entry: Union[str, List[str]]
-	) -> Optional[Union[str, int, float, bool, None, List, Dict]]:
-		...
+	def get(self, entry: None, ret_type: Any) -> Dict[str, Any]:
+		""" config.get(None, None/dict) """
 
 	@overload
-	def get(self, entry: Union[str, List[str]], ret_type: None, raise_exc: Literal[False]
-	) -> Optional[Union[str, int, float, bool, None, List, Dict]]:
-		...
+	def get(self, entry: None, ret_type: Any, *, raise_exc: bool) -> Dict[str, Any]:
+		""" config.get(None, None/dict, raise_exc=True/False) """
 
 	@overload
-	def get(self, entry: Union[str, List[str]], ret_type: None, raise_exc: Literal[True]
-	) -> Union[str, int, float, bool, None, List, Dict]:
-		...
+	def get(self, entry: None, *, raise_exc: bool) -> Dict[str, Any]:
+		""" config.get(None, raise_exc=True/False) """
+
+
+	# Overloads for method call with 'entry' but without return type
 
 	@overload
-	def get(self, entry: Union[str, List[str]], ret_type: Type[T]) -> Optional[T]:
-		...
+	def get(self, entry: Union[str, List[str]]) -> JSONTypes:
+		""" config.get('db') """
 
 	@overload
-	def get(self, entry: Union[str, List[str]], ret_type: Type[T], raise_exc: Literal[False]) -> Optional[T]:
-		...
+	def get(self, entry: Union[str, List[str]], ret_type: None) -> JSONTypes:
+		""" config.get('db', None) """
 
 	@overload
-	def get(self, entry: Union[str, List[str]], ret_type: Type[T], raise_exc: Literal[True]) -> T:
-		...
+	def get(self, entry: Union[str, List[str]], *, raise_exc: bool) -> JSONTypes:
+		""" config.get('db', raise_exc=False/True) """
 
-	def get(self,
+	@overload
+	def get(self, entry: Union[str, List[str]], ret_type: None, *, raise_exc: bool) -> JSONTypes:
+		""" config.get('db', None, raise_exc=False/True) """
+
+
+	# Overloads for method call with 'entry' and return type
+
+	@overload
+	def get(self, entry: Union[str, List[str]], ret_type: Type[JT]) -> Optional[JT]:
+		""" config.get('db', dict) """
+
+	@overload
+	def get(self, entry: Union[str, List[str]], ret_type: Type[JT], *, raise_exc: Literal[False]) -> Optional[JT]:
+		""" config.get('db', dict, raise_exc=False) """
+
+	@overload
+	def get(self, entry: Union[str, List[str]], ret_type: Type[JT], *, raise_exc: Literal[True]) -> JT:
+		""" config.get('db', dict, raise_exc=True) """
+
+
+	def get(self, # type: ignore[misc]
 		entry: Optional[Union[str, List[str]]] = None,
-		ret_type: Optional[Type[T]] = None,
-		raise_exc: bool = False
-	) -> Union[Dict[str, Any], T, Optional[T]]:
+		ret_type: Optional[Type[JT]] = None,
+		*, raise_exc: bool = False
+	) -> Union[JSONTypes, Optional[JT]]:
 		"""
 		Optional arguments:
 		:param ret_type: expected return type (str, int, Dict, List, ...).
@@ -90,22 +170,34 @@ class AmpelConfig:
 			ret = ret[el]
 
 		if ret_type:
-			if not isinstance(ret, ret_type):
+
+			if origin := get_origin(ret_type):
+				ret_type = origin
+
+			if not isinstance(ret, ret_type): # type: ignore[arg-type]
 				raise ValueError(
 					f"Retrieved value has not the expected type.\n"
 					f"Expected: {ret_type}\n"
 					f"Found: {type(ret)}"
 				)
 
-		return ret # type: ignore[return-value]
+		return ret
 
 
-	def print(self, entry: Optional[str] = None) -> None:
+	def print(self,
+		entry: Optional[str] = None, format: Literal['json', 'yaml'] = 'yaml'
+	) -> None:
+
+		out = self.get(entry)
 		print(
-			json.dumps(
-				self.get(entry), indent=4 # type: ignore[arg-type]
-			)
+			yaml.dump(out) if format == 'yaml'
+			else json.dumps(out, indent=4)
 		)
+
+
+	def freeze(self) -> None:
+		if not self.is_frozen():
+			self._config = Freeze.recursive_freeze(self._config)
 
 
 	def is_frozen(self) -> bool:
