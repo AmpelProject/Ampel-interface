ddbc2dc9935403a1a832a5d79d1d12f27887f725
vbrinnel <valery.brinnel@gmail.com>
2020-05-29 17:33:24 +0200

    Add __init__.py to ampel.base for easier imports

---
+++ ampel/base/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-interface/ampel/base/__init__.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 15.05.2020
+# Last Modified Date: 17.05.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+# flake8: noqa: F401
+from .AmpelABC import AmpelABC
+from .AmpelUnit import AmpelUnit
+from .decorator import abstractmethod, defaultmethod
