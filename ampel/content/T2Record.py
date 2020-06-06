0091c3e6b8f32aa5c77585c3c996dc0ac5bc89c8
vbrinnel <valery.brinnel@gmail.com>
2020-06-06 15:06:03 +0200

    T2Record: include T2SubRecord

--- ampel/content/T2Record.py
+++ ampel/content/T2Record.py
@@ -4,22 +4,32 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 13.01.2018
-# Last Modified Date: 10.03.2020
+# Last Modified Date: 06.06.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Sequence, Union, Optional, TypedDict, Dict
-from ampel.types import ChannelId, StockId
+from typing import Sequence, Union, Optional, TypedDict
+from ampel.type import ChannelId, StockId, DataPointId, Tag
+from ampel.content.T2SubRecord import T2SubRecord
+
+T2Links = Union[bytes, DataPointId, StockId]
+
 
 class T2Record(TypedDict, total=False):
 
+	# Indexed values
 	_id: bytes
-	unit: Union[int, str] # int to enable future potential hash optimizations
 	stock: Union[StockId, Sequence[StockId]]
-	link: Union[bytes, Sequence[bytes]]
-	tag: Optional[Sequence[Union[int, str]]]
+
+	# Compound index
+	unit: Union[int, str] # unit id can be hashed for performance reason
+	config: int
+	link: Union[T2Links, Sequence[T2Links]]
+
+	# Non-indexed values
+	tag: Optional[Sequence[Tag]]
 	channel: Sequence[ChannelId]
 	col: Optional[str]
-	run: Union[int, Sequence[int]]
+	run: Union[int, Sequence[int]] # positive, ever increasing integer
+	# Usually a T2RunState member but let's not be too restrictive here
 	status: int
-	config: Union[int, Dict]
-	result: Optional[Sequence[dict]] # value(s) returned by t2 unit execution(s)
+	body: Optional[Sequence[T2SubRecord]] # value(s) returned by t2 unit execution(s)
