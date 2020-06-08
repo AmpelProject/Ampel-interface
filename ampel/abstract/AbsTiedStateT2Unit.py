a26194670431f1cc5af6093bef383ccf9a4b0f9d
vbrinnel <valery.brinnel@gmail.com>
2020-06-08 19:06:06 +0200

    AbsTiedStateT2Unit update

--- ampel/abstract/AbsTiedStateT2Unit.py
+++ ampel/abstract/AbsTiedStateT2Unit.py
@@ -4,16 +4,21 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 11.03.2020
-# Last Modified Date: 11.03.2020
+# Last Modified Date: 08.06.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Union, Iterable, Sequence
-from ampel.abc import abstractmethod
+from typing import Union, Iterable, Sequence, TypedDict, Optional, Dict, Any
+from ampel.type import T2UnitResult
+from ampel.base import abstractmethod
 from ampel.abstract.AbsStateT2Unit import AbsStateT2Unit
 from ampel.content.Compound import Compound
 from ampel.content.DataPoint import DataPoint
 from ampel.content.T2Record import T2Record
-from ampel.t2.T2Result import T2Result
+
+
+class Dependency(TypedDict):
+	unit: str
+	config: Optional[Dict[str, Any]]
 
 
 class AbsTiedStateT2Unit(AbsStateT2Unit, abstract=True):
@@ -22,15 +27,15 @@ class AbsTiedStateT2Unit(AbsStateT2Unit, abstract=True):
 	Known sub-class: AbsTiedLightCurveT2
 	"""
 
-	dependency: Union[str, Sequence[str]]
+	dependency: Optional[Union[Dependency, Sequence[Dependency]]]
 
 	@abstractmethod
 	def run(self, # type: ignore
 		compound: Compound,
 		datapoints: Iterable[DataPoint],
 		t2_records: Sequence[T2Record]
-	) -> T2Result:
+	) -> T2UnitResult:
 		"""
-		Returned T2Result dataclass should contain computed science results to be saved into the DB.
+		Returned object should contain computed science results to be saved into the DB.
 		Notes: dict must have only string keys and values must be bson encodable
 		"""
