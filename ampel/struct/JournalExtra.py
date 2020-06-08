4a4628eb41dcfd25a8e04b0c1d217465cafd645c
vbrinnel <valery.brinnel@gmail.com>
2020-06-08 20:00:36 +0200

    JournalExtra update

---
+++ ampel/struct/JournalExtra.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-interface/ampel/struct/JournalExtra.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 15.10.2018
+# Last Modified Date: 08.06.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Dict, Optional, Sequence, Union, Any
+
+# Cannot import Tag from ampel.type because of cyclic import issues
+Tag = Union[int, str]
+
+
+class JournalExtra:
+	"""
+	Structure potentialy returned by ampel units to customize
+	the transient journal entry created after a process is run.
+	"""
+
+	__slots__ = "tag", "status", "extra"
+
+	def __init__(self,
+		status: Optional[int] = None,
+		tag: Optional[Union[Tag, Sequence[Tag]]] = None,
+		extra: Optional[Dict[str, Any]] = None
+	) -> None:
+		"""
+		:param status: potential integer status of the executed process
+		:param tag: journal entry tag(s)
+		:param extra: optional dict, which if provided, will be included 'as is' under journal root key 'extra'
+		Note regarding the type of extra: as of june 2020, mypy does not support recursive / json type
+		-> https://github.com/python/mypy/issues/731
+		"""
+		self.tag = tag
+		self.extra = extra
+		self.status = status
