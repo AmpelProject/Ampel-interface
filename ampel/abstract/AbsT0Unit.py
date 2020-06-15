49a6ef24468c51e2033273f784414088f46e0ced
vbrinnel <valery.brinnel@gmail.com>
2020-06-15 21:32:42 +0200

    AbsT0Unit update

--- ampel/abstract/AbsT0Unit.py
+++ ampel/abstract/AbsT0Unit.py
@@ -4,15 +4,15 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 07.10.2019
-# Last Modified Date: 14.03.2020
+# Last Modified Date: 15.06.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from ampel.abc import abstractmethod
-from ampel.abstract.AbsDataUnit import AbsDataUnit
-from ampel.content.DataPoint import DataPoint
 from typing import Any, List
+from ampel.base import abstractmethod, AmpelABC, DataUnit
+from ampel.content.DataPoint import DataPoint
 
-class AbsT0Unit(AbsDataUnit, abstract=True):
+
+class AbsT0Unit(AmpelABC, DataUnit, abstract=True):
 
 	@abstractmethod
 	def ampelize(self, arg: Any) -> List[DataPoint]:
