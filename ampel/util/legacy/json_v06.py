102bd1d582b793d816c0d30a4bce898f63eb4c9d
Jakob van Santen <jvansanten@gmail.com>
2020-09-03 22:40:32 +0200

    json_v06: deserialize old TransientViews

--- ampel/util/legacy/json_v06.py
+++ ampel/util/legacy/json_v06.py
@@ -1,13 +1,16 @@
 # Deserialize JSON representations of dataclasses from Ampel v0.6
 
 from importlib import import_module
-from typing import Any, Callable, Dict, Tuple
+from typing import Any, Callable, Dict, List, Tuple
 
 import bson.json_util
 
+from ampel.content.Compound import Compound
 from ampel.content.DataPoint import DataPoint
+from ampel.content.T2Record import T2Record
 from ampel.view.LightCurve import LightCurve  # type: ignore[import]
 from ampel.view.ReadOnlyDict import ReadOnlyDict
+from ampel.view.TransientView import TransientView
 
 legacy_classes: Dict[str, Callable[[str, Tuple, Dict[str, Any]], Any]] = {}
 
@@ -16,8 +19,8 @@ def object_hook(
     dct: Dict[str, Any], options=bson.json_util.STRICT_JSON_OPTIONS,
 ) -> Any:
     """
-	Deserialize an object serialized by AmpelEncoder
-	"""
+    Deserialize an object serialized by AmpelEncoder
+    """
     obj = bson.json_util.object_hook(dct, options)
     if type(obj) != type(dct):
         return obj
@@ -38,10 +41,122 @@ def upgrade(*fqn):
     return decorator
 
 
+_ampel_flags = {
+    0: "ZTF",
+}
+
+_ampel_photo_flags = {
+    20: "PHOTOPOINT",
+    21: "UPPERLIMIT",
+    22: "BAND_ZTF_G",
+    23: "BAND_ZTF_R",
+    24: "BAND_ZTF_I",
+    25: "ZTF_COLLAB",
+    26: "ZTF_PUB",
+}
+
+_ampel_tran_flags = {
+    20: "HAS_ERROR",
+    21: "T1_AUTO_COMPLETE",
+}
+
+_ampel_compound_flags = {
+    20: "HAS_UPPER_LIMITS",
+    21: "HAS_AUTOCOMPLETED_PHOTO",
+    22: "HAS_SUPERSEDED_PPS",
+    23: "HAS_EXCLUDED_PPS",
+    24: "HAS_MANUAL_EXCLUSION",
+    25: "HAS_DATARIGHTS_EXCLUSION",
+    26: "WITH_CUSTOM_POLICIES",
+    27: "ZTF_COLLAB_DATA",
+}
+
+
 @upgrade("ampel.base.flags.PhotoFlags.PhotoFlags")
-def flags(ctor: str, args: Tuple, kwargs: Dict[str, Any]) -> None:
-    # TODO: translate flags -> tags
-    return None
+def photoflags(ctor: str, args: Tuple, kwargs: Dict[str, Any]) -> List[str]:
+    return [
+        v
+        for k, v in {**_ampel_flags, **_ampel_photo_flags}.items()
+        if args[0] & (1 << k)
+    ]
+
+
+@upgrade("ampel.base.flags.TransientFlags.TransientFlags")
+def tranflags(ctor: str, args: Tuple, kwargs: Dict[str, Any]) -> List[str]:
+    return [
+        v
+        for k, v in {**_ampel_flags, **_ampel_tran_flags}.items()
+        if args[0] & (1 << k)
+    ]
+
+
+@upgrade("ampel.core.flags.CompoundFlags.CompoundFlags")
+def compflags(ctor: str, args: Tuple, kwargs: Dict[str, Any]) -> List[str]:
+    return [
+        v
+        for k, v in {**_ampel_flags, **_ampel_compound_flags}.items()
+        if args[0] & (1 << k)
+    ]
+
+
+@upgrade("ampel.base.Compound.Compound")
+def comp(ctor: str, args: Tuple, kwargs: Dict[str, Any]) -> Compound:
+    return Compound(
+        {
+            "_id": bytes(kwargs["id"]),
+            "added": kwargs["added"],
+            "tier": kwargs["tier"],
+            "stock": kwargs["stockId"],  # FIXME: translate old stockIds?
+            "len": kwargs["len"],
+            "tag": kwargs["alTags"],
+            "body": [
+                {"id": v, "tag": ["UPPERLIMIT" if k == "ul" else "PHOTOPOINT"]}
+                for entry in kwargs["comp"]
+                for k, v in entry.items()
+            ],
+        }
+    )
+
+
+@upgrade("ampel.base.ScienceRecord.ScienceRecord")
+def t2(ctor: str, args: Tuple, kwargs: Dict[str, Any]) -> T2Record:
+    return T2Record(
+        {
+            "_id": bytes(),
+            "stock": kwargs["tran_id"],  # FIXME: translate old stockIds?
+            "unit": kwargs["t2_unit_id"],
+            "link": [bytes(c) for c in kwargs["compound_id"]],
+            "body": [
+                {
+                    "version": entry["versions"]["py"],
+                    "ts": entry["dt"],
+                    "duration": entry["duration"],
+                    "run": entry["runId"],
+                    "result": entry["output"],
+                }
+                for entry in kwargs["results"]
+            ],
+        }
+    )
+
+
+@upgrade("ampel.base.TransientView.TransientView")
+def tview(ctor: str, args: Tuple, kwargs: Dict[str, Any]) -> TransientView:
+    return TransientView(
+        {
+            "id": kwargs["tran_id"],  # FIXME: translate old stockIds?
+            "extra": {},
+            "stock": {
+                "_id": kwargs["tran_id"],
+                "tag": kwargs["flags"],
+                "name": kwargs["tran_names"],
+                "journal": kwargs["journal"],
+                "channel": kwargs["channel"],
+            },
+            "t0": kwargs["photopoints"] + kwargs["upperlimits"],
+            "t2": kwargs["t2records"],
+        }
+    )
 
 
 @upgrade(
