0e479c92165fbd44127659991982986f1550fc24
vbrinnel <valery.brinnel@gmail.com>
2021-02-10 17:51:01 +0100

    T2Record: prevent mutliple stock association for now

--- ampel/content/T2Record.py
+++ ampel/content/T2Record.py
@@ -4,14 +4,14 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 13.01.2018
-# Last Modified Date: 06.06.2020
+# Last Modified Date: 10.02.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Sequence, Union, Optional, TypedDict
 from ampel.type import ChannelId, StockId, DataPointId, Tag
 from ampel.content.T2SubRecord import T2SubRecord
 
-T2Links = Union[bytes, DataPointId, StockId]
+T2Link = Union[bytes, DataPointId, StockId]
 
 
 class T2Record(TypedDict, total=False):
@@ -22,27 +22,35 @@ class T2Record(TypedDict, total=False):
 	:class:`~ampel.content.DataPoint.DataPoint`, or :class:`~ampel.content.Compound.Compound`.
 	"""
 
-	#: Database key
+	#: Database primary id
 	_id: bytes
+
 	#: Stock id associated with the data
-	stock: Union[StockId, Sequence[StockId]]
+	#: (multiple stock ids disabled for simplicity for now)
+	stock: StockId
 
 	#: Name of the unit to be run. This may be hashed for performance reasons.
 	unit: Union[int, str]
+
 	#: Configuration hash, if unit defaults were overridden. The underlying values can be resolved with
 	#: :meth:`UnitLoader.get_init_config() <ampel.core.UnitLoader.UnitLoader.get_init_config>`
 	config: Optional[int]
+
 	#: References to input data
-	link: Union[T2Links, Sequence[T2Links]]
+	link: Union[T2Link, Sequence[T2Link]]
 
 	tag: Optional[Sequence[Tag]]
 	channel: Sequence[ChannelId]
 
 	#: Name of the database collection holding the input data
+	#: (enables efficient T3 DB queries)
 	col: Optional[str]
+
 	#: Identifier of the process that created this record
 	run: Union[int, Sequence[int]]
+
 	#: A member of :class:`~ampel.t2.T2RunState.T2RunState`
 	status: int
+
 	#: value(s) returned by T2 unit execution(s)
 	body: Optional[Sequence[T2SubRecord]]
