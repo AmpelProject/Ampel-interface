a988dcde6d09bc953be5416de6a824354ca1a445
vbrinnel <valery.brinnel@gmail.com>
2021-02-12 12:20:00 +0100

    Update t2 run states

--- ampel/enum/T2RunState.py
+++ ampel/enum/T2RunState.py
@@ -12,12 +12,16 @@ from enum import IntEnum
 # flake8: noqa (E221)
 class T2RunState(IntEnum):
 	"""
-	Status values that can requested by T2 units (along with any positive value)
+	Status values usable by T2 units (along with any positive value)
 	Potential state of:
 		- :class:`~ampel.content.T2Document.T2Document`
 		- :class:`~ampel.content.T2Record.T2Record`
 	"""
 
-	COMPLETED            = 0
-	ERROR                = -1
-	UNIT_RUN_AGAIN       = -2
+	COMPLETED                 = 0
+	# First 1000 negative values are system reserved
+	ERROR                     = -1000
+	MISSING_INFO              = -1001
+	UNEXPECTED_DEPENDENCY     = -1002
+	RERUN_REQUESTED           = -1003
+	OUTDATED_CODE             = -1004
--- ampel/enum/T2SysRunState.py
+++ ampel/enum/T2SysRunState.py
@@ -13,26 +13,33 @@ from ampel.enum.T2RunState import T2RunState
 # flake8: noqa (E221)
 class T2SysRunState(IntEnum): # cannot subclass T2RunState unfortunately
 	"""
-	Status values reserved for the ampel system.
-	Typically set by ingesters or T2Processor.
+	Status values between 0 and -1000 are reserved for the ampel system.
+	(Typically set by ingesters or T2Processor)
 
 	Potential state of:
 		- :class:`~ampel.content.T2Document.T2Document`
 		- :class:`~ampel.content.T2Record.T2Record`
 	"""
 
-	COMPLETED            = 0
-	ERROR                = -1
-	UNIT_RUN_AGAIN       = -2
-	SYS_RUN_AGAIN        = -3
-	TO_RUN               = -4
-	TO_RUN_PRIO          = -5
-	QUEUED               = -6
-	RUNNING              = -7
-	EXPORTED             = -8
-	EXCEPTION            = -9
-	TOO_MANY_TRIALS      = -10
-	UNKNOWN_LINK         = -11
-	UNKNOWN_CONFIG       = -12
-	MISSING_DEPENDENCY   = -13
-	MISSING_INFO         = -14
+	COMPLETED                 = 0
+	NEW                       = -1
+	NEW_PRIO                  = -2  # For now, std ingesters do not support this
+	RUNNING                   = -3
+	PENDING_DEPENDENCY        = -4
+	EXCEPTION                 = -5
+	QUEUED                    = -6
+	EXPORTED                  = -7
+	TOO_MANY_TRIALS           = -8
+
+	# Might be due to ingester bugs
+	UNKNOWN_LINK              = -9
+	UNKNOWN_CONFIG            = -10
+	MISSING_DEPENDENCY        = -11
+
+	# Copied from T2RunState
+	# -> python does not support enum inheritance
+	ERROR                     = -1000
+	MISSING_INFO              = -1001
+	UNEXPECTED_DEPENDENCY     = -1002
+	RERUN_REQUESTED           = -1003
+	OUTDATED_CODE             = -1004
