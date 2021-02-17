226433a0da2006f1093db96e3cbd11c49b191144
vbrinnel <valery.brinnel@gmail.com>
2021-02-17 21:09:30 +0100

    AbsTiedPointT2Unit: allow dependencies on multiple other point T2s

--- ampel/abstract/AbsTiedPointT2Unit.py
+++ ampel/abstract/AbsTiedPointT2Unit.py
@@ -4,10 +4,10 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 16.02.2021
-# Last Modified Date: 16.02.2021
+# Last Modified Date: 17.02.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Dict, Union, Optional, Literal, Tuple
+from typing import Dict, Union, Optional, Literal, Tuple, Sequence
 from ampel.base import abstractmethod
 from ampel.type import T2UnitResult
 from ampel.abstract.AbsTiedT2Unit import AbsTiedT2Unit
@@ -31,7 +31,7 @@ class AbsTiedPointT2Unit(AbsTiedT2Unit, abstract=True):
 
 
 	@abstractmethod
-	def run(self, datapoint: DataPoint, t2_view: T2DocView) -> T2UnitResult:
+	def run(self, datapoint: DataPoint, t2_views: Sequence[T2DocView]) -> T2UnitResult:
 		"""
 		Returned object should contain computed science results to be saved into the DB.
 		Notes: dict must have only string keys and values must be bson encodable
