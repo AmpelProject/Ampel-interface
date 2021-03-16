0ed1c708934aa871fc4b9a725ba630209715d0c1
vbrinnel <valery.brinnel@gmail.com>
2021-03-16 13:29:16 +0100

    LogDocument: update field names and types

--- ampel/content/LogDocument.py
+++ ampel/content/LogDocument.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.02.2020
-# Last Modified Date: 01.03.2020
+# Last Modified Date: 16.03.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import sys
@@ -12,20 +12,68 @@ if sys.version_info.minor > 8:
 	from typing import TypedDict
 else:
 	from typing_extensions import TypedDict
-from typing import Sequence, Union, Optional, Any, Dict
+from typing import Sequence, Union, Any, Dict
 from ampel.type import ChannelId, StockId
+
+
+class ChannelLogEntry(TypedDict):
+	"""
+	Abbreviations:
+	s: stock, a: alert, f: flag, r: run, m: msg, c: channel
+
+	Used to save multiple channel specific messages into a single LogDocument
+	Example of a LogDocument embedding two ChannelLogEntry entries: {
+		"_id" : ObjectId("5be4aa6254048041edbac352"),
+		"s" : NumberLong(1810101032122523),
+		"a" : NumberLong(404105201415015004),
+		"f" : 572784643,
+		"r" : 509,
+		"m" : [
+			{"c" : "NO_FILTER", "m": "Alert accepted"},
+			{"c" : "HU_RANDOM", "m": "Alert accepted"},
+		]
+	}
+	"""
+	#: channel
+	c: ChannelId
+
+	#: msg
+	m: str
+
 
 class LogDocument(TypedDict, total=False):
 	"""
-	A message produced by an Ampel unit during processing.
+	Abbreviations:
+	s: stock, a: alert, f: flag, r: run, m: msg, c: channel
 
-	This is a dict containing 1 or more of the following items:
+	Example: {
+		"_id" : ObjectId("5be4aa6254048041edbac353"),
+		"s" : NumberLong(1810101032122523),
+		"a" : NumberLong(404105201415015004),
+		"f" : 572784643,
+		"r" : 509,
+		"c" : "NO_FILTER",
+		"m" : "Alert accepted"
+	}
 	"""
+
 	#: database key
 	_id: bytes
-	flag: int
-	run: Union[int, Sequence[int]]
-	msg: Optional[Union[str, Sequence[str]]]
-	stock: Optional[Union[StockId, Sequence[StockId]]]
-	channel: Optional[Union[ChannelId, Sequence[ChannelId]]]
-	extra: Optional[Dict[str, Any]]
+
+	#: flag
+	f: int
+
+	#: run id
+	r: Union[int, Sequence[int]]
+
+	#: msg
+	m: Union[str, Sequence[str], ChannelLogEntry]
+
+	#: stock
+	s: Union[StockId, Sequence[StockId]]
+
+	#: channel
+	c: Union[ChannelId, Sequence[ChannelId]]
+
+	#: extra
+	e: Dict[str, Any]
