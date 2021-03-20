d8ca89f12717c92eb71ec0722463a0b6f64b6100
vbrinnel <valery.brinnel@gmail.com>
2021-03-20 11:43:38 +0100

    New model: FlatAnyOf
    Imported from Ampel-core

---
+++ ampel/model/operator/FlatAnyOf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-interface/ampel/model/operator/FlatAnyOf.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 15.10.2018
+# Last Modified Date: 18.03.2021
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+import collections
+from typing import List, Generic
+from pydantic import validator
+from pydantic.generics import GenericModel
+from ampel.type import T
+
+class FlatAnyOf(GenericModel, Generic[T]):
+	"""
+	Similar to AnyOf except that it does not allow embedded AllOf elements
+	"""
+	any_of: List[T]
+
+	@validator('any_of', pre=True)
+	def cast_to_list(cls, v):
+		if not isinstance(v, collections.abc.Sequence):
+			return [v]
+		return v
