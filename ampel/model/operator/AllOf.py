3625bf8a876bff743b0a57544038ed1ec7e399b8
vbrinnel <valery.brinnel@gmail.com>
2021-03-20 11:42:39 +0100

    New model: AllOf
    Imported from Ampel-core

---
+++ ampel/model/operator/AllOf.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-interface/ampel/model/operator/AllOf.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 15.10.2018
+# Last Modified Date: 18.03.2021
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from pydantic.generics import GenericModel
+from typing import List, Generic
+from ampel.type import T
+
+
+class AllOf(GenericModel, Generic[T]):
+	#: Select items by logical AND
+	all_of: List[T]
