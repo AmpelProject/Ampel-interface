1efa8eaa746e44251f57f5d9ac68cb79201df2d4
vbrinnel <valery.brinnel@gmail.com>
2021-04-03 18:03:20 +0200

    ampel.ingest -> ampel.compile

--- ampel/compile/CompoundBluePrint.py
+++ ampel/compile/CompoundBluePrint.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-core/ampel/ingest/CompoundBluePrint.py
+# File              : Ampel-core/ampel/compile/CompoundBluePrint.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 01.01.2018
