6c68a4909079e698734b428d14b5a9ddac137725
vbrinnel <valery.brinnel@gmail.com>
2021-05-03 22:10:46 +0200

    Rename JournalTweak -> JournalAttributes

--- ampel/struct/JournalAttributes.py
+++ ampel/struct/JournalAttributes.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-interface/ampel/struct/JournalTweak.py
+# File              : Ampel-interface/ampel/struct/JournalAttributes.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 15.10.2018
-# Last Modified Date: 08.06.2020
+# Last Modified Date: 03.05.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Dict, Optional, Sequence, Union, Any
-
-# Cannot import Tag from ampel.type because of cyclic import issues
-Tag = Union[int, str]
+from ampel.type import Tag
 
 
-class JournalTweak:
+class JournalAttributes:
 	"""
 	Structure potentialy returned by Ampel units to customize
-	the transient journal entry created after a process is run.
+	the stock journal entry created after a process is run.
 	"""
 
-	__slots__ = "tag", "status", "extra"
+	__slots__ = 'tag', 'code', 'extra'
 
-	#: status code of the executed process
-	status: Optional[int]
+	#: code / status
+	code: Optional[int]
+
 	#: journal entry tag(s)
 	tag: Optional[Union[Tag, Sequence[Tag]]]
+
 	#: if provided, will be included as-is under the journal root key 'extra'
 	extra: Optional[Dict[str, Any]]
 
 	def __init__(self,
-		status: Optional[int] = None,
+		code: Optional[int] = None,
 		tag: Optional[Union[Tag, Sequence[Tag]]] = None,
 		extra: Optional[Dict[str, Any]] = None
 	) -> None:
 		"""
-		:param status: potential integer status of the executed process
+		:param code: potential integer code of the executed process
 		:param tag: journal entry tag(s)
 		:param extra: optional dict, which if provided, will be included 'as is' under journal root key 'extra'
 		Note regarding the type of extra: as of june 2020, mypy does not support recursive / json type
@@ -42,4 +42,4 @@ class JournalTweak:
 		"""
 		self.tag = tag
 		self.extra = extra
-		self.status = status
+		self.code = code
