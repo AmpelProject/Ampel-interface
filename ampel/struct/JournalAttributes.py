03625aa3dbd0f1af5fdd0e3fbcb45a9a342a39ba
vbrinnel <valery.brinnel@gmail.com>
2021-07-24 11:15:14 +0200

    JournalAttributes: add methods dict() and into()

--- ampel/struct/JournalAttributes.py
+++ ampel/struct/JournalAttributes.py
@@ -4,16 +4,17 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 15.10.2018
-# Last Modified Date: 03.05.2021
+# Last Modified Date: 12.05.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Dict, Optional, Sequence, Union, Any
-from ampel.type import Tag
+from ampel.types import Tag
+from ampel.content.JournalRecord import JournalRecord
 
 
 class JournalAttributes:
 	"""
-	Structure potentialy returned by Ampel units to customize
+	Structure potentialy used by Ampel units to customize
 	the stock journal entry created after a process is run.
 	"""
 
@@ -27,6 +28,7 @@ class JournalAttributes:
 
 	#: if provided, will be included as-is under the journal root key 'extra'
 	extra: Optional[Dict[str, Any]]
+
 
 	def __init__(self,
 		code: Optional[int] = None,
@@ -43,3 +45,31 @@ class JournalAttributes:
 		self.tag = tag
 		self.extra = extra
 		self.code = code
+
+
+	def dict(self) -> JournalRecord:
+		d: JournalRecord = {}
+		if self.tag:
+			d['tag'] = self.tag
+		if self.code:
+			d['code'] = self.code
+		if self.extra:
+			d['extra'] = self.extra
+		return d
+			
+
+	def into(self, prime: JournalRecord) -> JournalRecord:
+
+		if self.code:
+			prime['code'] = self.code
+
+		if self.tag:
+			prime['tag'] = self.tag
+
+		if self.extra:
+			if 'extra' in prime:
+				prime['extra'] = self.extra | prime['extra']
+			else:
+				prime['extra'] = self.extra
+
+		return prime
