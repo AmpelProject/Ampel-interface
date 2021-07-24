15ef582a4c099806e0d0573564d382f6d3954897
vbrinnel <valery.brinnel@gmail.com>
2021-07-24 10:49:39 +0200

    JournalRecord: add traceid

--- ampel/content/JournalRecord.py
+++ ampel/content/JournalRecord.py
@@ -4,42 +4,46 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 12.12.2019
-# Last Modified Date: 08.06.2020
+# Last Modified Date: 22.06.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-import sys
-if sys.version_info.minor > 8:
-	from typing import TypedDict
-else:
-	from typing_extensions import TypedDict
-from typing import Sequence, Union, Literal, Any, Dict, List
-from ampel.type import ChannelId, Tag
+from typing import Sequence, Union, Literal, Any, Dict, TypedDict
+from ampel.types import ChannelId, Tag
+
 
 class JournalRecord(TypedDict, total=False):
 	"""
-	A record of activity on a stock.
+	A record of activity on a stock document.
 
-	.. seealso:: :class:`~ampel.struct.JournalTweak.JournalTweak`
-
-	This is a dict containing 1 or more of the following items:
+	.. seealso:: :class:`~ampel.struct.JournalAttributes.JournalAttributes`
 	"""
+
 	#: Tier of the associated process
 	tier: Literal[-1, 0, 1, 2, 3]
+
 	#: UNIX epoch of the activity
 	ts: Union[int, float]
+
 	#: Channels associated with the activity
 	channel: Union[ChannelId, Sequence[ChannelId]]
+
 	#: Name of the associated process
 	process: Union[int, str]
+
 	#: Free-form labels
 	tag: Union[Tag, Sequence[Tag]]
+
+	#: Run(s) associated with this record
+	run: int
+
 	#: Status code of the associated process
 	code: int
-	#: id of the unit associated with this record
-	unit: Union[int, str]
-	#: id of the document associated with the invocation
-	doc: Union[int, bytes]
-	#: run(s) associated with this record
-	run: Union[int, List[int]]
+
+	#: Duration of the process
+	duration: Union[int, float]
+
+	#: Trace ids
+	traceid: Dict[str, int]
+
 	#: Free-form information
 	extra: Dict[str, Any]
