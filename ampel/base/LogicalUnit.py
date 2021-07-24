926f84789133c1cca2c75b74533af99a30758ebb
vbrinnel <valery.brinnel@gmail.com>
2021-07-24 11:26:41 +0200

    LogicalUnit: v0.8 update

--- ampel/base/LogicalUnit.py
+++ ampel/base/LogicalUnit.py
@@ -1,58 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-interface/ampel/base/DataUnit.py
+# File              : Ampel-interface/ampel/base/LogicalUnit.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 12.10.2019
-# Last Modified Date: 18.12.2020
-# Last Modified By  : Jakob van Santen <jakob.van.santen@desy.de>
+# Last Modified Date: 22.06.2021
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Tuple, Dict, Any, Optional, Union, ClassVar
+from typing import Tuple, Dict, Any, Optional, ClassVar
 from ampel.protocol.LoggerProtocol import LoggerProtocol
 from ampel.base.AmpelBaseModel import AmpelBaseModel
+from ampel.abstract.Secret import Secret
 
 
-class DataUnit(AmpelBaseModel):
+class LogicalUnit(AmpelBaseModel):
+	"""
+	Logical as in: performing logic operations of ampel's tier: add, combine, augment, synthetise.
+	Base for standardized t0, t1, t2 and t3 units.
+	Note that the defined parameters must all be serializable
+	"""
 
-	#: Named resources required by this unit.
-	#: These are passed on to subclasses.
+	#: Resources requirements as class variable (passed on to and merged with subclasses).
 	require: ClassVar[Optional[Tuple[str, ...]]] = None
-	version: Optional[Union[str, float]] = None
 
-	#: Logging handle, provided at runtime.
-	logger: LoggerProtocol
+	#: a dictionary containing session information, which can be requested in process configuration.
+	#: Example of session information: date and time the current process was last run
+	session_info: Optional[Dict[str, Any]] = None
+
+	#: Private variable potentially set by UnitLoader for provenance purposes. Either:
+	#: * 0 if provanance flag is False
+	#: * -1 in case model content is not serializable
+	#: * any other signed int value
+	_trace_id: int = 0
 
 	# Some unit contributors might want to restrict units usage
 	# by scoping them to their respective distribution name (str)
-	# private: Optional[str] = None
+	# private: ClassVar[Optional[str]] = None
+
 
 	@classmethod
 	def __init_subclass__(cls, **kwargs) -> None:
-		"""
-		Inherit `require` attribute from superclass
-		"""
 		super().__init_subclass__(**kwargs) # type: ignore
-		if (require := getattr(cls, "require", None)) or isinstance(require, tuple):
-			combined_require = set(sum(
-				(
-					r for base in cls.__bases__
-					if isinstance(r := getattr(base, 'require', None), tuple)
-				),
-				tuple()
-			)).union(require)
-			setattr(cls, "require", tuple(combined_require))
+		setattr(cls, "resource",
+			set(getattr(cls, "resource", None) or []) | {
+				el for base in cls.__bases__
+				for el in (getattr(base, 'resource', None) or [])
+			}
+		)
 
 
 	def __init__(self,
-		resource: Optional[Dict[str, Any]] = None, **kwargs
+		logger: LoggerProtocol,
+		resource: Optional[Dict[str, Any]] = None,
+		**kwargs
 	) -> None:
+		"""
+		Note: logical units are initialized (by UnitLoader) as follows:
+		* ctor
+		* if available, secrets are resolved
+		* if defined by sub-class, post_init() is called
+		"""
+
+		if logger is None:
+			raise ValueError("Parameter logger cannot be None")
+
 		AmpelBaseModel.__init__(self, **kwargs)
-		#: Named resources configured for this unit. The keys are the elements of :attr:`require`.
-		self.resource: Optional[Dict[str, Any]] = resource
 
-		if hasattr(self, "post_init"):
-			self.post_init() # type: ignore
+		d = self.__dict__
+		self._trace_content = {
+			k: d[k] for k in sorted(d)
+			if not isinstance(d[k], Secret) and
+			k not in ("session_info", "logger")
+		}
 
+		self.logger = logger
 
-	def get_version(self) -> Optional[Union[str, float]]:
-		return self.version
+		if resource:
+			# self._trace_content['require'] = self.require
+			self.resource = resource
