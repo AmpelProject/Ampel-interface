f81758f8a09b647bc3342c9ef3c643a57ac00af3
vbrinnel <valery.brinnel@gmail.com>
2021-07-24 11:27:17 +0200

    New abstract class: Secret

---
+++ ampel/abstract/Secret.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-interface/ampel/abstract/Secret.py
+# License           : BSD-3-Clause
+# Author            : Jakob van Santen <jakob.van.santen@desy.de>
+# Date              : 14.08.2020
+# Last Modified Date: 20.06.2021
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Generic
+from ampel.types import T
+from ampel.base.decorator import abstractmethod
+from ampel.base.AmpelABC import AmpelABC
+
+
+class Secret(Generic[T], AmpelABC, abstract=True):
+    """
+    A wrapper for a piece of sensitive data, e.g. a password or access token.
+    """
+
+    @abstractmethod
+    def get(self) -> T:
+        """ Returns secret value """
+
+    @abstractmethod
+    def set(self, arg: T) -> None:
+        """ Sets secret value """
