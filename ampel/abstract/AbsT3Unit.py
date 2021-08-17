01db7ccb3c5c1d5212734e58a379bb62384f45d4
vbrinnel <valery.brinnel@gmail.com>
2021-08-17 15:47:36 +0200

    AbsT3Unit: v0.8 update

--- ampel/abstract/AbsT3Unit.py
+++ ampel/abstract/AbsT3Unit.py
@@ -4,48 +4,46 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 23.02.2018
-# Last Modified Date: 08.06.2020
+# Last Modified Date: 17.06.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Tuple, Optional, Union, Dict, Any, TypeVar, Generic
-from ampel.type import StockId
-from ampel.base import abstractmethod, defaultmethod, AmpelABC, DataUnit
+from typing import Union, TypeVar, ClassVar, Type, Generic, Generator
+from ampel.types import UBson
 from ampel.view.SnapView import SnapView
-from ampel.struct.JournalTweak import JournalTweak
+from ampel.base.AmpelABC import AmpelABC
+from ampel.base.decorator import abstractmethod
+from ampel.base.LogicalUnit import LogicalUnit
+from ampel.struct.UnitResult import UnitResult
+from ampel.struct.JournalAttributes import JournalAttributes
 
 T = TypeVar("T", bound=SnapView)
 
 
-class AbsT3Unit(Generic[T], AmpelABC, DataUnit, abstract=True):
+class AbsT3Unit(Generic[T], AmpelABC, LogicalUnit, abstract=True):
 	"""
-	General generic abstract class for T3 units.
-	Inherit this class if your unit is compatible with multiple type of views,
-	that is if you access only SnapView fields or perform dedicated isinstance checks.
+	Generic abstract class for T3 units.
 
-	:param context:
-	  a dictionary containing context information, which can be
-	  requested in the process configuration. Examples of context information are:
+	:param session_info:
+	  (defined in LogicalUnit)
+	  a dictionary containing session information, which can be
+	  requested in the process configuration. Examples of session information are:
 	  
 	    - Date and time the current process was last run
 	    - Number of alerts processed since then
 	  
 	  .. note::
-	    Custom context information can be added to this dict by
-	    implementing a subclass of
-	    :class:`~ampel.t3.context.AbsT3RunContextAppender.AbsT3RunContextAppender`
+	    Custom session information can be added to this dict by implementing a subclass of
+	    :class:`~ampel.session.AbsSessionInfo.AbsSessionInfo`
 	    and adding the custom unit to the process configuration
 	"""
 
-	context: Optional[Dict[str, Any]]
+	# avoid introspection at run-time
+	_View: ClassVar[Type[T]] = SnapView # type: ignore[assignment]
 
 
 	@abstractmethod
-	def add(self, views: Tuple[T, ...]) -> Optional[Union[JournalTweak, Dict[StockId, JournalTweak]]]:
-		""" Implementing T3 units get SnapView (or subclasses of SnapView) instances via this method """
-		...
-
-
-	@defaultmethod
-	def done(self) -> None:
-		""" Method called after all data have been processed """
-		pass
+	def process(self, gen: Generator[T, JournalAttributes, None]) -> Union[UBson, UnitResult]:
+		"""
+		Implementing T3 units receive SnapView instances (or subclasses of) via this method.
+		Use gen.send(JournalAttributes) to customize the journal of the stock document associated with a specific view.
+		"""
