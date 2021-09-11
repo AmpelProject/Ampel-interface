a92de4d885c1ef455d6d631a4e1963d2535701a3
vbrinnel <valery.brinnel@gmail.com>
2021-09-11 15:13:55 +0200

    UnitResult: allow tag customization

--- ampel/struct/UnitResult.py
+++ ampel/struct/UnitResult.py
@@ -4,32 +4,40 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 04.05.2021
-# Last Modified Date: 17.06.2021
+# Last Modified Date: 11.09.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Optional
-from ampel.types import UBson
+from typing import Optional, Union, Sequence
+from ampel.types import UBson, Tag
 from ampel.struct.JournalAttributes import JournalAttributes
 
 
 class UnitResult:
 	"""
 	Structure potentialy returned by Ampel units to customize
-	document code, meta or the stock journal entry.
+	document body, code, tag or the stock journal entry.
 	"""
 
-	__slots__ = 'body', 'code', 'journal'
+	__slots__ = 'body', 'tag', 'code', 'journal'
 
 	def __init__(self,
 		body: UBson = None,
+		tag: Optional[Union[Tag, Sequence[Tag]]] = None,
 		code: Optional[int] = None,
 		journal: Optional[JournalAttributes] = None
 	) -> None:
 		"""
-		:param body: content for the field 'body'  of the associated tier document
-		:param code: customize DocumentCode (ex: DocumentCode.RERUN_REQUESTED or any int)
+		:param body: content for the field 'body' of the associated tier document
+		:param tag: tag(s) to be added to the content of the field 'tag' of the associated tier document.
+		Note that tags are 'public' attributes (they are not channel bound) and as such, they:
+		- are visible by any projection
+		- can be used for any db queries
+		:param code: customize 'code' of the associated tier document:
+		- Either a member of `ampel.enum.DocumentCode` such as DocumentCode.RERUN_REQUESTED (negative values)
+		- Or any positive integer number (custom defined for a particular ampel unit)
 		:param journal: customization of the journal entry associated with the stock document
 		"""
 		self.body = body
+		self.tag = tag
 		self.code = code
 		self.journal = journal
