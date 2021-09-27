043a2a5fb73e94695ef8d2f7fa29239c20975f21
vbrinnel <valery.brinnel@gmail.com>
2021-09-28 01:02:58 +0200

    AbsPointT2Unit: docstring update

--- ampel/abstract/AbsPointT2Unit.py
+++ ampel/abstract/AbsPointT2Unit.py
@@ -4,51 +4,46 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 28.12.2019
-# Last Modified Date: 03.04.2021
+# Last Modified Date: 28.09.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Union, Optional, ClassVar
+from typing import Union
 from ampel.types import UBson
 from ampel.struct.UnitResult import UnitResult
 from ampel.content.DataPoint import DataPoint
 from ampel.base.AmpelABC import AmpelABC
 from ampel.base.decorator import abstractmethod
 from ampel.base.LogicalUnit import LogicalUnit
-from ampel.model.T2IngestOptions import T2IngestOptions
 
 
 class AbsPointT2Unit(AmpelABC, LogicalUnit, abstract=True):
 	"""
 	A T2 unit bound to a :class:`~ampel.content.DataPoint.DataPoint`
+
+	Note that the implementing class can customize the default ingestion behavior
+	(which :class:`~ampel.content.DataPoint.DataPoint` to create
+	:class:`T2 documents <ampel.content.T2Document.T2Document>` for)
+	by defining the class variable 'eligible'
+	
+	Example::
+	  ingest: ClassVar[DPSelection] = DPSelection(select=(1, -2, 5))
+	
+	will create documents bound to every 5th datapoint starting from the 2nd
+	and ending with the 3rd-to-last
+
+	Example::
+	   ingest: ClassVar[DPSelection] = DPSelection(filter='PPSFilter', sort='jd', select='first'}
+	
+	select options:
+	  - "first": first datapoint for a stock
+	  - "last": most recent datapoint for a stock
+	  - "all": every datapoint (default)
+	  - :class:`tuple`: :class:`slice` of datapoints
+	-> see DPSelection docstring for info regarding 'filter' and 'sort'.
+
+	If 'eligible' is not specified, default ingestion will occur:
+	a T2 document will be created for each datapoint
 	"""
-
-	# See T2IngestOptions docstring for more info
-	#: Which :class:`~ampel.content.DataPoint.DataPoint` to create
-	#: :class:`T2 documents <ampel.content.T2Document.T2Document>` for
-	#:
-	#: Example::
-	#:    {'filter': 'PPSFilter', 'sort': 'jd', 'select': 'first'}
-	#:
-	#: "first"
-	#:   first datapoint for a stock
-	#: "last"
-	#:   most recent datapoint for a stock
-	#: "all"
-	#:   every datapoint (default)
-	#: :class:`tuple`
-	#:   :class:`slice` of datapoints
-	#:
-	#: Example::
-	#:
-	#:   {"select": (1, -2, 5)}
-	#:
-	#: will create documents bound to every 5th datapoint starting from the 2nd
-	#: and ending with the 3rd-to-last
-	#:
-	#: If unspecified, a T2 document will be created for each datapoint.
-
-	ingest: ClassVar[Optional[T2IngestOptions]] = None
-
 
 	@abstractmethod
 	def process(self, datapoint: DataPoint) -> Union[UBson, UnitResult]:
