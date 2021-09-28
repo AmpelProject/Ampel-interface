79a4dabca9333aaca2d83fd9afb07d41b2778421
vbrinnel <valery.brinnel@gmail.com>
2021-09-28 02:56:26 +0200

    DPSelection: add method tools()

--- ampel/model/DPSelection.py
+++ ampel/model/DPSelection.py
@@ -7,8 +7,12 @@
 # Last Modified Date: 28.09.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Union, Optional, Literal, Tuple
+from typing import Union, Optional, Literal, Tuple, Callable
+from ampel.model.UnitModel import UnitModel
 from ampel.model.StrictModel import StrictModel
+from ampel.base.AuxUnitRegister import AuxUnitRegister
+from ampel.abstract.AbsApplicable import AbsApplicable
+
 
 class DPSelection(StrictModel):
 
@@ -39,3 +43,38 @@ class DPSelection(StrictModel):
 
 		if self.sort and not self.select:
 			raise ValueError("Options 'sort' requires option 'select'")
+
+
+	def tools(self) -> Tuple[Optional[AbsApplicable], Optional[Callable[[list], list]], slice]:
+
+		so = None
+		sl = None
+
+		f = AuxUnitRegister.new_unit(
+			UnitModel(unit=self.filter),
+			sub_type = AbsApplicable
+		) if self.filter else None
+
+		if self.sort:
+			if self.sort == 'id':
+				lbd = lambda k: k['id']
+			else:
+				lbd = lambda k: k['body'][self.sort]
+			so = lambda l: sorted(l, key=lbd)
+
+		if self.select is None:
+			sl = slice(None)
+		elif self.select == "all":
+			sl = slice(None)
+		elif self.select == "first":
+			sl = slice(1)
+		elif self.select == "last":
+			sl = slice(-1, -2, -1)
+		elif isinstance(self.select, (list, tuple)) and len(self.select) == 3:
+			sl = slice(*self.select)
+		else:
+			raise ValueError(
+				f"Unsupported value provided as slice parameter : {self.select}"
+			)
+
+		return f, so, sl
