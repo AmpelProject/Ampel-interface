a4fcdfc8f407766b78ae113754d1b4e0ea5dae7b
vbrinnel <valery.brinnel@gmail.com>
2021-09-28 02:55:35 +0200

    Retire get_link() from tied abstract classes

--- ampel/abstract/AbsTiedStateT2Unit.py
+++ ampel/abstract/AbsTiedStateT2Unit.py
@@ -4,10 +4,10 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 11.03.2020
-# Last Modified Date: 03.04.2021
+# Last Modified Date: 28.09.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Sequence, Dict, Any, Union, Optional, Generic
+from typing import Sequence, Union, Generic
 from ampel.types import UBson, T
 from ampel.struct.UnitResult import UnitResult
 from ampel.view.T2DocView import T2DocView
@@ -25,32 +25,6 @@ class AbsTiedStateT2Unit(Generic[T], AbsTiedT2Unit, abstract=True):
 	"""
 
 	t2_dependency: Sequence[StateT2Dependency[T]]
-
-	@staticmethod
-	@abstractmethod(force=True)
-	def get_link(
-		link_override: Dict[str, Any],
-		compound: T1Document,
-		datapoints: Sequence[DataPoint]
-	) -> Optional[Union[int, bytes]]:
-		"""
-		Method used by T2Processor if 't2_dependency' is specified in the t2 config dict
-		and if 'link_override' is defined in there.
-
-		T2Processor needs to retrieve the T2Records of units tied with this unit.
-		If unspecified in config, t2 dependencies are resolved - for each unit defined in get_tied_unit_names() -
-		using the db match query: {unit: <unit_name>, config: None, link: <same link as root doc>}.
-
-		This behavior is overridable/customizable by adding the keyword 't2_dependency' to the t2 config dict.
-		The value of t2_dependency should be of type Union[T2Dependency, List[T2Dependency]].
-		The parameter 'link_override' in T2Dependency allows to link this state T2 with a different value
-		than the one registered as 'link' in the T2Record (which is the _id of a compound document for state t2s).
-
-		This method thus enables to tie a state T2 with the result of a point t2
-		(the returned link could be the id of the first datapoint contained in the compound)
-
-		:returns: the value of 'link' (tied t2 document) to be matched
-		"""
 
 	@abstractmethod
 	def process(self,
