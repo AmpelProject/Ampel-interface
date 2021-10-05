16ebae55e42082361b2355d33be4c3a87ae3cfdb
vbrinnel <valery.brinnel@gmail.com>
2021-10-05 13:26:26 +0200

    MetaActionCode: new tag codes

--- ampel/enum/MetaActionCode.py
+++ ampel/enum/MetaActionCode.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 01.09.2021
-# Last Modified Date: 05.09.2021
+# Last Modified Date: 01.10.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from enum import IntFlag
@@ -20,11 +20,13 @@ class MetaActionCode(IntFlag):
 	EXTRA_META                = 4
 	EXTRA_JOURNAL             = 8
 	SET_CODE                  = 16
-	ADD_TAG                   = 32
-	PULL_TAG                  = 64
-	ADD_CHANNEL               = 128
-	PULL_CHANNEL              = 1<<8
-	ADD_BODY                  = 1<<9
-	PULL_BODY                 = 1<<10
-	RESET_BODY                = 1<<11
-	BUMP_STOCK_UPD            = 1<<12
+	ADD_INGEST_TAG            = 64
+	ADD_UNIT_TAG              = 128
+	ADD_OTHER_TAG             = 1<<8
+	PULL_TAG                  = 1<<9
+	ADD_CHANNEL               = 1<<10
+	PULL_CHANNEL              = 1<<11
+	ADD_BODY                  = 1<<12
+	PULL_BODY                 = 1<<13
+	RESET_BODY                = 1<<14
+	BUMP_STOCK_UPD            = 1<<15
