404d94fca48eee765087f27e848b4ffc85e0e745
vbrinnel <valery.brinnel@gmail.com>
2021-10-10 12:57:36 +0200

    MetaActivity: declare new fields

--- ampel/content/MetaActivity.py
+++ ampel/content/MetaActivity.py
@@ -4,11 +4,11 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 07.10.2021
-# Last Modified Date: 08.10.2021
+# Last Modified Date: 10.10.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Sequence, Union, TypedDict
-from ampel.types import ChannelId, Tag
+from typing import Sequence, Union, TypedDict, List
+from ampel.types import ChannelId, Tag, DataPointId
 from ampel.enum.MetaActionCode import MetaActionCode
 
 
@@ -20,6 +20,12 @@ class MetaActivity(TypedDict, total=False):
 
 	#: Free-form labels
 	tag: Union[Tag, Sequence[Tag]]
+
+	#: references dps exclusion (t1)
+	excl: List[DataPointId]
+
+	#: Document code (useful when t1_compute is performed on the fly)
+	code: int
 
 	#: Channel(s) associated with action
 	channel: Union[ChannelId, Sequence[ChannelId]]
