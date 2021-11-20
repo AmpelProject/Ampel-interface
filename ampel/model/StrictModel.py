78ccac33af728c8f425c6d36209a295773240c7a
vbrinnel <valery.brinnel@gmail.com>
2021-11-20 23:06:45 +0100

    StrictModel: remove AmpelConfig dependency

--- ampel/model/StrictModel.py
+++ ampel/model/StrictModel.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-core/ampel/model/StrictModel.py
+# File              : Ampel-interface/ampel/model/StrictModel.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 30.09.2018
-# Last Modified Date: 14.05.2020
+# Last Modified Date: 19.11.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from pydantic import BaseModel, BaseConfig, Extra
-from ampel.config.AmpelConfig import AmpelConfig
+_check_types = True
+
 
 class StrictModel(BaseModel):
 	"""
@@ -20,7 +21,7 @@ class StrictModel(BaseModel):
 	In []: %timeit A(a=12)
 	9.97 µs ± 66.9 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
 
-	In []: AmpelConfig._check_types=0
+	In []: _check_types=0
 
 	In []: %timeit A(a=12)
 	3.71 µs ± 36.9 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
@@ -36,7 +37,7 @@ class StrictModel(BaseModel):
 	def __init__(self, **kwargs) -> None:
 		""" Raises validation errors if extra fields are present """
 
-		if AmpelConfig._check_types:
+		if _check_types:
 			self.__config__.extra = Extra.forbid
 			BaseModel.__init__(self, **kwargs)
 			self.__config__.extra = Extra.allow
