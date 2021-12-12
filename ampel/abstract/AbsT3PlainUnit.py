b10e1b83aa2adae2c2bf56b1c5fe8b7086e7feeb
vbrinnel <valery.brinnel@gmail.com>
2021-12-12 17:05:28 +0100

    AbsT3PlainUnit: typing update

--- ampel/abstract/AbsT3PlainUnit.py
+++ ampel/abstract/AbsT3PlainUnit.py
@@ -4,10 +4,10 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 08.12.2021
-# Last Modified Date: 08.12.2021
+# Last Modified Date: 12.12.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Union, Optional
+from typing import Union
 from ampel.types import UBson
 from ampel.view.T3Store import T3Store
 from ampel.base.AmpelABC import AmpelABC
@@ -20,9 +20,9 @@ class AbsT3PlainUnit(AmpelABC, LogicalUnit, abstract=True):
 	""" Generic abstract class for T3 units receiving only a T3Store via the process method """
 
 	@abstractmethod
-	def process(self, t3s: Optional[T3Store] = None) -> Union[UBson, UnitResult]:
+	def process(self, t3s: T3Store) -> Union[UBson, UnitResult]:
 		"""
-		Optional parameter t3s provides a t3 store containing t3 views.
-		The content of the store is dependent on the configuration of the 'supply' option
-		of the underlying t3 process config.
+		The content of the t3 store is dependent on:
+		- the configuration of the 'include' option of the underlying t3 process
+		- previously run t3 units if the option 'propagate' is activated
 		"""
