186263885f1e2494a6f63b8acc61e80262640359
vbrinnel <valery.brinnel@gmail.com>
2021-12-21 18:14:30 +0100

    util.serialize.walk_and_encode: add 'destructive' option

--- ampel/util/serialize.py
+++ ampel/util/serialize.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 25.03.2021
-# Last Modified Date: 25.03.2021
+# Last Modified Date: 20.12.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Any
@@ -18,25 +18,36 @@ except ImportError:
  
 dsi = dict.__setitem__
 
-def walk_and_encode(arg: Any) -> Any:
+def walk_and_encode(arg: Any, destructive: bool = True) -> Any:
 	"""
 	Convert bytes values into "b64:<string>"
 	Convert ObjectId instances into "oid:<hex>"
-	Note: destructive op: modifies the input dict(s)
+	:param destructive: true modifies the input dict(s)
 	"""
 
 	if isinstance(arg, (list, tuple, set)):
-		return [walk_and_encode(el) for el in arg]
+		return [walk_and_encode(el, destructive) for el in arg]
 
 	elif isinstance(arg, dict):
-		for k, v in arg.items():
-			if HAVE_BSON and isinstance(v, ObjectId):
-				dsi(arg, k, "oid:" + v.binary.hex())
-			elif isinstance(v, bytes):
-				dsi(arg, k, "b64:" + b64encode(v).decode('ascii'))
-			else:
-				dsi(arg, k, walk_and_encode(v))
-		return arg
+		if destructive:
+			for k, v in arg.items():
+				if HAVE_BSON and isinstance(v, ObjectId):
+					dsi(arg, k, "oid:" + v.binary.hex())
+				elif isinstance(v, bytes):
+					dsi(arg, k, "b64:" + b64encode(v).decode('ascii'))
+				else:
+					dsi(arg, k, walk_and_encode(v, destructive))
+			return arg
+		else:
+			return {
+				k: ("oid:" + v.binary.hex())
+				if HAVE_BSON and isinstance(v, ObjectId) else (
+					("b64:" + b64encode(v).decode('ascii'))
+					if isinstance(v, bytes)
+					else walk_and_encode(v, destructive)
+				)
+				for k, v in arg.items()
+			}
 
 	elif HAVE_BSON and isinstance(arg, ObjectId):
 		return "oid:" + arg.binary.hex()
