17a9b221425be011b8ded762f018604ae21c73de
vbrinnel <valery.brinnel@gmail.com>
2021-12-31 12:14:02 +0100

    AbsT3ReviewUnit: minor

--- ampel/abstract/AbsT3ReviewUnit.py
+++ ampel/abstract/AbsT3ReviewUnit.py
@@ -4,10 +4,10 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 23.02.2018
-# Last Modified Date: 12.12.2021
+# Last Modified Date: 27.12.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Union, TypeVar, ClassVar, Type, Generic, Generator
+from typing import Union, TypeVar, Type, Generic, Generator
 from ampel.types import UBson, T3Send
 from ampel.view.SnapView import SnapView
 from ampel.view.T3Store import T3Store
@@ -23,7 +23,7 @@ class AbsT3ReviewUnit(Generic[T], AmpelABC, LogicalUnit, abstract=True):
 	""" Generic abstract class for T3 units receiving a SnapView generator """
 
 	# avoid introspection at run-time
-	_View: ClassVar[Type[T]] = SnapView # type: ignore[assignment]
+	_View: Type[T] = SnapView # type: ignore[assignment]
 
 	@abstractmethod
 	def process(self,
