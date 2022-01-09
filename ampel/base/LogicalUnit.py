8a8350f492d928829c702b04ae754af82706f525
vbrinnel <valery.brinnel@gmail.com>
2022-01-09 22:04:36 +0100

    LogicalUnit: remove __init__
    _trace_content dict is now built on demand via private method in AmpelUnit

--- ampel/base/LogicalUnit.py
+++ ampel/base/LogicalUnit.py
@@ -4,21 +4,25 @@
 # License:             BSD-3-Clause
 # Author:              valery brinnel <firstname.lastname@gmail.com>
 # Date:                12.10.2019
-# Last Modified Date:  13.12.2021
+# Last Modified Date:  09.01.2022
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 from typing import Any, ClassVar
-from ampel.types import Traceless, TRACELESS
-from ampel.protocol.LoggerProtocol import LoggerProtocol
+from ampel.types import Traceless
 from ampel.base.AmpelUnit import AmpelUnit
-from ampel.secret.Secret import Secret
+from ampel.protocol.LoggerProtocol import LoggerProtocol
 
 
 class LogicalUnit(AmpelUnit):
 	"""
 	Logical as in: performing logic operations of ampel's tier: add, combine, augment, synthetise.
 	Base for standardized t0, t1, t2 and t3 units.
-	Note that the defined parameters must all be serializable
+	Note:
+	- the defined parameters must be serializable
+	- logical units are initialized (by UnitLoader) as follows:
+	* ctor
+	* if available, secrets are resolved
+	* if defined by sub-class, post_init() is called
 	"""
 
 	logger: Traceless[LoggerProtocol]
@@ -48,23 +52,3 @@ class LogicalUnit(AmpelUnit):
 				for el in (getattr(base, 'require', None) or [])
 			}
 		)
-
-
-	def __init__(self, **kwargs) -> None:
-		"""
-		Note: logical units are initialized (by UnitLoader) as follows:
-		* ctor
-		* if available, secrets are resolved
-		* if defined by sub-class, post_init() is called
-		"""
-
-		super().__init__(**kwargs)
-		self._trace_content = dict(
-			sorted(
-				self.dict(
-					exclude_unset=False,
-					exclude_defaults=False
-				).items()
-			)
-		)
-
