7049419615ebc4352d843baa21f2397840579e41
valery brinnel <valery.brinnel@gmail.com>
2022-04-21 00:07:49 +0200

    Bump version: v0.8.3a10

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extras_require = {
 
 setup(
 	name = 'ampel-interface',
-	version = '0.8.3-alpha.9',
+	version = '0.8.3-alpha.10',
 	description = 'Base classes for the Ampel analysis platform',
 	long_description = '# Ampel-interface\n\n`ampel-interface` provides type-hinted abstract base classes for [Ampel](https://ampelproject.github.io).',
 	author = 'Valery Brinnel',
