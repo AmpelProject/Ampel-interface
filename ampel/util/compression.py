e782bcec5eb8d694dca6b85df57f2c20a28fae7e
valery brinnel <valery.brinnel@gmail.com>
2022-04-20 13:16:04 +0200

    util.compression: renames

--- ampel/util/compression.py
+++ ampel/util/compression.py
@@ -4,7 +4,7 @@
 # License:             BSD-3-Clause
 # Author:              valery brinnel <firstname.lastname@gmail.com>
 # Date:                29.06.2021
-# Last Modified Date:  29.06.2021
+# Last Modified Date:  20.04.2022
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 import zipfile
@@ -17,10 +17,10 @@ def compress(
 	payload: bytes,
 	filename: str,
 	alg: TCompression = "ZIP_DEFLATED",
-	compress_level: int = 9
+	compression_level: int = 9
 ) -> bytes:
 
-	outbio, zf = _new(alg, compress_level)
+	outbio, zf = _new_zip_file(alg, compression_level)
 	zf.writestr(filename, payload)
 	zf.close()
 	return outbio.getvalue()
@@ -29,10 +29,10 @@ def compress(
 def compress_many(
 	arg: dict[str, bytes],
 	alg: TCompression = "ZIP_DEFLATED",
-	compress_level: int = 9
+	compression_level: int = 9
 ) -> bytes:
 
-	outbio, zf = _new(alg, compress_level)
+	outbio, zf = _new_zip_file(alg, compression_level)
 	for k, v in arg.items():
 		zf.writestr(k, v)
 	zf.close()
@@ -40,15 +40,15 @@ def compress_many(
 	return outbio.getvalue()
 
 
-def _new(
+def _new_zip_file(
 	alg: TCompression = "ZIP_DEFLATED",
-	compress_level: int = 9
+	compression_level: int = 9
 ) -> tuple[BytesIO, zipfile.ZipFile]:
 
 	outbio = BytesIO()
 	return outbio, zipfile.ZipFile(
 		outbio, "w", getattr(zipfile, alg), False,
-		compresslevel = compress_level
+		compresslevel = compression_level
 	)
 
 
