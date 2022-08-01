2e6ac761d51ceb3bef837065b1c68ff798c6e0f7
valery brinnel <valery.brinnel@gmail.com>
2022-08-01 17:26:19 +0200

    MaybeIntAction: handle potential quotes

--- ampel/cli/MaybeIntAction.py
+++ ampel/cli/MaybeIntAction.py
@@ -4,7 +4,7 @@
 # License:             BSD-3-Clause
 # Author:              valery brinnel <firstname.lastname@gmail.com>
 # Date:                17.03.2021
-# Last Modified Date:  18.03.2021
+# Last Modified Date:  31.07.2022
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 from argparse import Action
@@ -12,5 +12,8 @@ from argparse import Action
 class MaybeIntAction(Action):
 
 	def __call__(self, parser, namespace, values, option_string=None):
-		v = [int(el) if el.lstrip("-+").isdigit() else el for el in values]
+		v = [
+			int(el) if el.lstrip("-+").isdigit() else el
+			for el in [el.strip("'").strip('"') for el in values]
+		]
 		setattr(namespace, self.dest, v[0] if len(v) == 1 else v)
