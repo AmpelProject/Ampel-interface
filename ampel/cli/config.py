757c9ffe3006306ad8477c3af8361bd5a6ade567
valery brinnel <valery.brinnel@gmail.com>
2022-12-18 12:17:42 +0100

    cli.config: generate path within conda env if applicable

--- ampel/cli/config.py
+++ ampel/cli/config.py
@@ -7,23 +7,34 @@
 # Last Modified Date:  20.08.2022
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
-import os
+from os import path, environ, makedirs
 from appdirs import user_data_dir # type: ignore[import]
 
 def get_user_data_config_path() -> str:
+	"""
+	:returns: full path to yaml ampel conf
+	"""
+
+	if env := environ.get('CONDA_PREFIX'):
+
+		app_path = path.join(env, 'share')
+		if not path.exists(app_path):
+			raise ValueError('Conda path does not contain a shared folder?')
+
+		app_path = path.join(app_path, 'ampel')
+		if not path.exists(app_path):
+			makedirs(app_path)
+
+		# ex: /Users/hu/miniconda3/envs/myCondaEnv/share/ampel/conf.yml
+		return path.join(app_path, "conf.yml")
 
 	app_path = user_data_dir("ampel")
-	if not os.path.exists(app_path):
-		os.makedirs(app_path)
+	if not path.exists(app_path):
+		makedirs(app_path)
 
-	app_path = os.path.join(app_path, "conf")
-	if not os.path.exists(app_path):
-		os.makedirs(app_path)
+	app_path = path.join(app_path, "conf")
+	if not path.exists(app_path):
+		makedirs(app_path)
 
-	env = os.environ.get('CONDA_DEFAULT_ENV')
-	if env:
-		app_path = os.path.join(app_path, env)
-		if not os.path.exists(app_path):
-			os.makedirs(app_path)
-
-	return os.path.join(app_path, "conf.yml")
+	# ex: /Users/hu/Library/Application Support/ampel/conf.yml
+	return path.join(app_path, "conf.yml")
