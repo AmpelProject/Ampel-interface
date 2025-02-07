2c19f6ac822185917cfff17d7b069e165ee90940
Jakob van Santen <jvansanten@gmail.com>
2025-02-07 09:26:46 +0100

    NamedSecret: resolve with validation hook

--- ampel/secret/NamedSecret.py
+++ ampel/secret/NamedSecret.py
@@ -7,9 +7,19 @@
 # Last Modified Date:  30.12.2021
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
+import contextlib
+from typing import TYPE_CHECKING, ClassVar
+
+from pydantic import model_validator
+
 from ampel.base.AmpelBaseModel import AmpelBaseModel
 from ampel.secret.Secret import Secret, T
 
+if TYPE_CHECKING:
+	from collections.abc import Generator
+	from typing import Self
+
+	from ampel.secret.AmpelVault import AmpelVault
 
 class NamedSecret(AmpelBaseModel, Secret[T]):
 	"""
@@ -33,3 +43,26 @@ class NamedSecret(AmpelBaseModel, Secret[T]):
 		if self.value:
 			raise ValueError("Secret already resolved")
 		self.value = arg
+
+	_vault: ClassVar["None | AmpelVault"] = None
+
+	@model_validator(mode="after")
+	def post_validate(self) -> "Self":
+		if self._vault is not None:
+			ValueType = (args[0]
+				if ((args := self.get_model_args()) and isinstance(args[0], type))
+				else object
+			)
+			if not self._vault.resolve_secret(self, ValueType):
+				raise TypeError(f"Could not resolve secret {self.label!r} as {getattr(ValueType, '__name__', '<untyped>')}")
+		return self
+
+	@classmethod
+	@contextlib.contextmanager
+	def resolve_with(cls, vault: "None | AmpelVault") -> "Generator[None]":
+		prev_vault = cls._vault
+		try:
+			cls._vault = vault
+			yield
+		finally:
+			cls._vault = prev_vault
