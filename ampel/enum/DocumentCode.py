64604b81b47f93528e0fcd906b9a3c566a832c3d
Jakob van Santen <jvansanten@gmail.com>
2026-01-27 12:43:45 +0100

    Add DocumentCode.T2_INADEQUATE_DEPENDENCY
    Tied T2 units may use this to indicate that the expected dependency was resolved, but the body content did not meet its selection criteria

--- ampel/enum/DocumentCode.py
+++ ampel/enum/DocumentCode.py
@@ -57,6 +57,7 @@ class DocumentCode(IntEnum):
 	T2_MISSING_INFO           = -2008 # ingester bugs, or uncommitted updates
 	T2_OUTDATED_CODE          = -2009 # unit returned unexpected type
 	T2_FAILED_DEPENDENCY      = -2010 # dependency encountered a permanent error
+	T2_INADEQUATE_DEPENDENCY  = -2011 # dependency does not meet the unit's criteria
 
 	# T3
 	T3_CONTEXT_ERROR          = -3000 # error occured in context stage
